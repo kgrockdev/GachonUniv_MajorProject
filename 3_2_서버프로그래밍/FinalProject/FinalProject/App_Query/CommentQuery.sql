8cc8a01741f0f4b98196e14e95b55611fff6c3a7
KyungRock Kim <124982026+kgrockdev@users.noreply.github.com>
2023-09-28 23:26:32 +0900

    Add files via upload

---
+++ 3_2_서버프로그래밍/FinalProject/FinalProject/App_Query/CommentQuery.sql
@@ -0,0 +1,14 @@
+﻿use Community
+
+Create table comment (
+	commentCode int IDENTITY (1, 1) primary key,
+	commentPostCode int not null,
+	commentUser nvarchar(20) not null,
+	commentPassword nvarchar(20) not null,
+	comment nvarchar(300) not null,
+	commentAuth nvarchar(20) not null
+)
+
+select * from comment
+
+drop table comment
